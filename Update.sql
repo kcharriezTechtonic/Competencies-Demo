3628b9062b46addcc368f0949360bbcbc821391d
kcharriezTechtonic <48114186+kcharriezTechtonic@users.noreply.github.com>
2019-06-20 11:51:01 -0600

    Add files via upload

---
+++ Update.sql
@@ -0,0 +1,24 @@
+CREATE TABLE Adventurers (
+   id INTEGER, 
+   name TEXT, 
+   level INTEGER,
+   class TEXT,
+   race TEXT,
+   background TEXT,
+   player TEXT,
+);
+
+INSERT INTO Adventurers (id, name, level, class, race, background, player) 
+VALUES (1, 'Glenn', 1, 'Wizard', 'Half-Elf', 'Outlander', 'Gene Bara'); 
+
+INSERT INTO Adventurers (id, name, level, class, race, background, player) 
+VALUES (2, 'Karla Grinn', 1, 'Rogue', 'Tiefling', 'Criminal', 'Stu Yates'); 
+
+INSERT INTO Adventurers (id, name, level, class, race, background, player) 
+VALUES (3, 'Barry Barbello', 1, 'Barbarian', 'Halfling', 'Entertainer', 'Shawn Murphy'); 
+
+ UPDATE Adventurers 
+SET notes = 'New Player' 
+WHERE id = 1; 
+
+SELECT * FROM Adventurers;
